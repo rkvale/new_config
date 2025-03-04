1265b1dbda93a028fa59dbb77ef29aa6364244c0
Rune Kvale <rune@kvale.io>
2025-03-04 08:37:34 +0100

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  outputs =
+    inputs:
+    let
+      inherit (inputs.nixpkgs) lib;
+      hosts = [
+        "knights"
+        "leafs"
+        "donald"
+      ];
+    in
+    {
+      nixosConfigurations = lib.genAttrs hosts (
+        host:
+        lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            ./nixos
+            ./hosts/${host}
+          ];
+        }
+      );
+    };
+}
